a14345d4729e5ba7dd9f9177a9fd4eb5db95029d
cafehaine <kilian.guillaume@gmail.com>
2018-03-29 15:07:17 +0000

    Fix crash on invalid mimetype, yapf pass

--- script.py
+++ script.py
@@ -30,12 +30,22 @@ def generateDirPage(path):
     ls.close()
 
     data = HTML_BEFORE
-    data += "<h1>"+path+"</h1>"
+    data += "<h1>" + path + "</h1>"
     data += "<a href=\"../\">../</a><br>"
     for entry in entries:
         data += "<a href=\"" + entry + "\">" + entry + "</a><br>"
     data += HTML_AFTER
     return data
+
+
+def getMimetype(path):
+    val = "text/plain"
+    try:
+        val = mimetypes.types_map[os.path.splitext(path)[1]]
+    except:
+        pass
+    return val
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     binded = False
@@ -44,7 +54,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((HOST, PORT))
             binded = True
         except Exception:
-            PORT+=1
+            PORT += 1
 
     print("Listening on http://127.0.0.1:" + str(PORT))
     s.listen(1)
@@ -62,18 +72,27 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 lines = request.splitlines()
                 method, path, protocol = lines[0].split(" ")
                 print("\t" + method + " " + path)
-                conn.send('HTTP/1.0 200 OK\r\n'.encode())
-                if path.endswith("/"):
-                    conn.send(("Content-Type: " + mimetypes.types_map[".html"] + "\r\n\r\n").encode())
-                    data = generateDirPage(path)
-                    conn.send(data.encode())
-                else:
-                    data = open(PATH + path, mode="rb")
-                    conn.send(("Content-Type: " + mimetypes.types_map[os.path.splitext(path)[1]] + "\r\n\r\n").encode())
-                    l = data.read(1024)
-                    while (l):
-                        conn.send(l)
+                if os.path.exists(PATH + path):
+                    conn.send('HTTP/1.0 200 OK\r\n'.encode())
+                    if path.endswith("/"):
+                        conn.send(
+                            ("Content-Type: " + mimetypes.types_map[".html"] +
+                             "\r\n\r\n").encode())
+                        data = generateDirPage(path)
+                        conn.send(data.encode())
+                    else:
+                        data = open(PATH + path, mode="rb")
+                        conn.send(("Content-Type: " + getMimetype(path) +
+                                   "\r\n\r\n").encode())
                         l = data.read(1024)
-                conn.close()
+                        while (l):
+                            conn.send(l)
+                            l = data.read(1024)
+                    conn.close()
+                else:  # File does not exist
+                    conn.send("HTTP/1.0 404 OK\r\n".encode())
+                    conn.send("Content-Type: text/plain\r\n\r\n".encode())
+                    conn.send("err 404".encode())
+                    conn.close()
         except socket.timeout:
             print("timeout")
