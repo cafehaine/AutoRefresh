8ddce9692034996517953392d227eae1f033112b
cafehaine <kilian.guillaume@gmail.com>
2018-03-30 12:15:31 +0000

    httpmanager: move outside of script

---
+++ httpmanager.py
@@ -0,0 +1,84 @@
+import os
+import mimetypes
+import sys
+
+HTML_BEFORE = """<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf8">
+  <title>RefresHTML</title>
+ </head>
+ <body>"""
+
+HTML_AFTER = """ </body>
+</html>"""
+
+mimetypes.init()
+PATH = os.path.realpath(os.path.dirname(sys.argv[0]))
+CWD = os.getcwd()
+
+# Javascript to be included at the end of each html documents
+_js_source = open(PATH + "/included.js", "r")
+JAVASCRIPT = "<script>" + _js_source.read() + "</script>"
+_js_source.close()
+
+def __generateDirPage__(path):
+    ls = os.scandir(CWD + path)
+    entries = []
+    for entry in ls:
+        entries.append(entry.name if entry.is_file() else (entry.name + "/"))
+    ls.close()
+
+    data = HTML_BEFORE
+    data += "<h1>" + path + "</h1>"
+    data += "<a href=\"../\">../</a><br>"
+    for entry in entries:
+        data += "<a href=\"" + entry + "\">" + entry + "</a><br>"
+    data += HTML_AFTER
+    return data
+
+
+def __getMimetype__(path):
+    val = "text/plain"
+    try:
+        val = mimetypes.types_map[os.path.splitext(path)[1]]
+    except:
+        pass
+    return val
+
+def handlehttp(conn, path):
+    if os.path.exists(CWD + path):
+        conn.send(b"HTTP/1.0 200 OK\r\n")
+        # Directory
+        if path.endswith("/"):
+            conn.send(
+                ("Content-Type: " + mimetypes.types_map[".html"] +
+                 "\r\n\r\n").encode())
+            data = __generateDirPage__(path)
+            conn.send(data.encode())
+        # File
+        else:
+            mime = __getMimetype__(path)
+            # HTML -> insert custom JS
+            if mime == "text/html":
+                data = open(CWD + path, mode="r")
+                content = data.read()
+                data.close()
+                content = content.replace("</body>",
+                                          JAVASCRIPT + "</body>")
+                conn.send((
+                    "Content-Type: " + mime + "\r\n\r\n").encode())
+                conn.send(content.encode())
+            # Send file as-is
+            else:
+                data = open(CWD + path, mode="rb")
+                conn.send((
+                    "Content-Type: " + mime + "\r\n\r\n").encode())
+                conn.send(data.read())
+                data.close()
+    # 404
+    else:
+        conn.send(b"HTTP/1.0 404 OK\r\n")
+        conn.send(b"Content-Type: text/plain\r\n\r\n")
+        conn.send(b"err 404")
+    conn.close()
--- script.py
+++ script.py
@@ -2,63 +2,16 @@
 
 import pyinotify
 import socket
-import os
 from webbrowser import open as webopen
-import mimetypes
-import sys
 # used to treat request arguments
 import re
 from websocketmanager import websocketmanager
+from httpmanager import handlehttp
 
-mimetypes.init()
-PATH = os.path.realpath(os.path.dirname(sys.argv[0]))
-CWD = os.getcwd()
 HOST = "0.0.0.0"
 BASEPORT = 8000
 
-HTML_BEFORE = """<!DOCTYPE HTML>
-<html>
- <head>
-  <meta charset="utf8">
-  <title>RefresHTML</title>
- </head>
- <body>"""
-
-HTML_AFTER = """ </body>
-</html>"""
-
 WEBSOCKETS = []
-
-# Javascript to be included at the end of each html documents
-_js_source = open(PATH + "/included.js", "r")
-JAVASCRIPT = "<script>" + _js_source.read() + "</script>"
-_js_source.close()
-
-
-def generateDirPage(path):
-    ls = os.scandir(CWD + path)
-    entries = []
-    for entry in ls:
-        entries.append(entry.name if entry.is_file() else (entry.name + "/"))
-    ls.close()
-
-    data = HTML_BEFORE
-    data += "<h1>" + path + "</h1>"
-    data += "<a href=\"../\">../</a><br>"
-    for entry in entries:
-        data += "<a href=\"" + entry + "\">" + entry + "</a><br>"
-    data += HTML_AFTER
-    return data
-
-
-def getMimetype(path):
-    val = "text/plain"
-    try:
-        val = mimetypes.types_map[os.path.splitext(path)[1]]
-    except:
-        pass
-    return val
-
 
 # Taken from https://stackoverflow.com/a/28950776/2279323
 def getLanIp():
@@ -74,6 +27,7 @@ def getLanIp():
 
 
 LANIP = getLanIp()
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     binded = False
     while not binded:
@@ -122,42 +76,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     websock = websocketmanager(conn, arguments)
                     WEBSOCKETS.append(websock)
                 # File/Directory
-                elif os.path.exists(CWD + path):
-                    conn.send(b"HTTP/1.0 200 OK\r\n")
-                    # Directory
-                    if path.endswith("/"):
-                        conn.send(
-                            ("Content-Type: " + mimetypes.types_map[".html"] +
-                             "\r\n\r\n").encode())
-                        data = generateDirPage(path)
-                        conn.send(data.encode())
-                    # File
-                    else:
-                        mime = getMimetype(path)
-                        # HTML -> insert custom JS
-                        if mime == "text/html":
-                            data = open(CWD + path, mode="r")
-                            content = data.read()
-                            data.close()
-                            content = content.replace("</body>",
-                                                      JAVASCRIPT + "</body>")
-                            conn.send((
-                                "Content-Type: " + mime + "\r\n\r\n").encode())
-                            conn.send(content.encode())
-                        # Send file as-is
-                        else:
-                            data = open(CWD + path, mode="rb")
-                            conn.send((
-                                "Content-Type: " + mime + "\r\n\r\n").encode())
-                            conn.send(data.read())
-                            data.close()
-                    conn.close()
-                # 404
                 else:
-                    conn.send(b"HTTP/1.0 404 OK\r\n")
-                    conn.send(b"Content-Type: text/plain\r\n\r\n")
-                    conn.send(b"err 404")
-                    conn.close()
+                    handlehttp(conn, path)
             print("done")
         except socket.timeout:
             print("timeout")
